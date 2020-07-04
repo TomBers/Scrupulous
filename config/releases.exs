00ad0d7ebbc50aac45d86e1817e22cc6236ffdd7
Tom Berman <tjcberman@gmail.com>
2020-07-04 10:53:08 +0100

    Get ready for deploy

---
+++ config/releases.exs
@@ -0,0 +1,11 @@
+import Config
+
+db_url = System.get_env("DATABASE_URL")
+
+config :scrupulous, ScrupulousWeb.Endpoint,
+       secret_key_base: "LkrIIPvPC9XgMqkhgH1RsOn+CyJ+Th/dVRKSxxjE86A+oeEPIal9Gr4DXOffUKjR"
+
+# Configure your database
+config :scrupulous, Scrupulous.Repo,
+       url: db_url,
+       pool_size: 15
