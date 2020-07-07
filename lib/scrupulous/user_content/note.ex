ef80e702aeae2f74ab57cda57d777c6631652f88
Tom Berman <tjcberman@gmail.com>
2020-07-07 20:50:26 +0100

    Associate note with user

--- lib/scrupulous/user_content/note.ex
+++ lib/scrupulous/user_content/note.ex
@@ -2,12 +2,14 @@ defmodule Scrupulous.UserContent.Note do
   use Ecto.Schema
   import Ecto.Changeset
   alias Scrupulous.StaticContent.Book
+  alias Scrupulous.Accounts.User
 
   schema "notes" do
     field :end_line, :integer
     field :note, :string
     field :start_line, :integer
     belongs_to :book, Book
+    belongs_to :user, User
 
     timestamps()
   end
@@ -15,7 +17,7 @@ defmodule Scrupulous.UserContent.Note do
   @doc false
   def changeset(note, attrs) do
     note
-    |> cast(attrs, [:start_line, :end_line, :note, :book_id])
+    |> cast(attrs, [:start_line, :end_line, :note, :book_id, :user_id])
     |> validate_required([:start_line, :end_line, :note])
   end
 end
