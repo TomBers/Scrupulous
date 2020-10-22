4bb3b410a0614d6c557ccf6a231f27794da9b866
Tom Berman <tjcberman@gmail.com>
2020-10-22 14:41:34 +0100

    Remove Store test

--- lib/scrupulous/store/store_test.ex
+++
@@ -1,9 +0,0 @@
-defmodule StoreTest do
-
-  def run(keys) do
-#    pid = Store.pid
-#    keys |> Enum.map(fn(key) -> Store.put(pid, key, 1..100000 |> Enum.map(fn(x) -> Enum.random(0..x) end)) end)
-#    Store.calc_memory_mb(pid)
-  end
-
-end
