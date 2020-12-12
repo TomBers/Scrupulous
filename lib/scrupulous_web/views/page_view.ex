d7bf747928e98dd51637380b9865676519280e9f
Tom Berman <tjcberman@gmail.com>
2020-12-12 15:50:48 +0000

    Add discuss button and change layout

--- lib/scrupulous_web/views/page_view.ex
+++ lib/scrupulous_web/views/page_view.ex
@@ -6,4 +6,12 @@ defmodule ScrupulousWeb.PageView do
     |> Enum.filter(fn(resource) -> resource.category == category end)
   end
 
+  def has_discussion_link(resources) do
+    Enum.any?(get_category_links(resources, Scrupulous.Constants.discussion()))
+  end
+
+  def discussion_link(resources) do
+    Enum.find(resources, fn(resource) -> resource.category == Scrupulous.Constants.discussion() end)
+  end
+
 end
